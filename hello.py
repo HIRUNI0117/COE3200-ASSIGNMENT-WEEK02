9b22f1cde219e875a967fb7f3b33f43432479ac6
Hiruni Kaushalya <hirunikaushalya0117@gmail.com>
2024-12-24 13:32:56 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+# A simple Python script to check if a number is even or odd
+
+# Ask for the user's number
+number = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print(f"The number {number} is even.")
+else:
+    print(f"The number {number} is odd.")
